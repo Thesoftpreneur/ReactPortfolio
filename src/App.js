e9b4e69d561f2ebabe4ee9f7f591ec8771312fb7
Jason Kraszewski <jkraszewski@algotrader.com>
2022-10-24 20:23:30 +0200

    add sample bootstrap template and add experiment with sample hooks from https://www.youtube.com/watch?v=TNhaISOUy6Q

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
+import {Toast, Button} from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+
 import './App.css';
 
-function App() {
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
+  const [count, setCount] = useState(0);
+
+  useEffect(() =>{
+    console.log("count variable is changed and is now equal:" + count);
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+      <Toast show={show} onClose={() => toggleShow(false)}>
+        <Toast.Header>
+          <strong className="mr-auto">React-Bootstrap</strong>
+        </Toast.Header>
+        <Toast.Body>{children}</Toast.Body>
+        <>{count}</>
+      </Toast>
+      <Button onClick={() => setCount(count + 1)}>Counter: {count}</Button>
+      <Button onClick={() => setCount(0)}>Reset counter</Button>
+    </>
   );
-}
+};
+
+const App = () => {
+  
+  return(
+  <Container className="p-3">
+    <Container className="p-5 mb-4 bg-light rounded-3">
+      <h1 className="header">Welcome To React-Bootstrap</h1>
+      <ExampleToast>
+        We now have Toasts
+        <span role="img" aria-label="tada">
+          🎉
+        </span>
+      </ExampleToast>
+    </Container>
+  </Container>
+);
+};
 
 export default App;
